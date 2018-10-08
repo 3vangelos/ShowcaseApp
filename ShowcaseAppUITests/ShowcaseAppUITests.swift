cb04fa059e5017dc69a8eb31330f8c1491aabda9
3vangelos <evangelos@posteo.eu>
2018-10-08 11:22:27 +0200

    added some Sample UI Tests to test navigation and orientation change

---
+++ ShowcaseAppUITests/ShowcaseAppUITests.swift
@@ -0,0 +1,40 @@
+import XCTest
+
+class ShowcaseAppUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+    var tablesQuery: XCUIElementQuery!
+    
+    override func setUp() {
+
+        app = XCUIApplication()
+        tablesQuery = app.tables
+        
+        continueAfterFailure = false
+        app.launch()
+        
+        XCUIDevice.shared.orientation = .portrait
+    }
+
+    override func tearDown() {
+        tablesQuery = nil
+        app = nil
+    }
+
+    func testNavigationToDetailFromFirstCell() {
+        tablesQuery.cells.firstMatch.tap()
+    }
+
+    func testNavigationBackFromDetailScreen() {
+        tablesQuery.cells.firstMatch.tap()
+        let usersButton = app.navigationBars["Posts"].buttons["Users"]
+        usersButton.tap()
+    }
+    
+    func testUIFlowAfterOrientationChange() {
+        XCUIDevice.shared.orientation = .landscapeLeft
+        tablesQuery.cells.firstMatch.tap()
+        let usersButton = app.navigationBars["Posts"].buttons["Users"]
+        usersButton.tap()
+    }
+}

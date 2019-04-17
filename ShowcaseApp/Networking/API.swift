19d7603499539771b3a8006c9d54a3f873cbff2e
3vangelos <evangelos@posteo.eu>
2019-04-17 11:58:08 +0200

    minor refactoring.

--- ShowcaseApp/Networking/API.swift
+++ ShowcaseApp/Networking/API.swift
@@ -3,18 +3,24 @@ import RxSwift
 
 final class API: APIProtocol {
     
+    private let fileHandler = FileHandler()
+    
     func getUsers() -> Observable<[User]> {
-        return FileHandler().read(from: "users").catchError { _ in
-            return Request.data(Resource.users.asURLRequest()).flatMap { data in
-                return Response.parse(data: data, to: "users")
+        let fileName = "users"
+        return FileHandler().read(from: fileName).catchError { _ in
+            return Request.data(Resource.users.asURLRequest()).flatMap { data -> Observable<[User]> in
+                self.fileHandler.write(data, to: fileName)
+                return Response.parse(data: data)
             }
         }
     }
     
     func getPostsByUserId(_ id: Int) -> Observable<[Post]> {
-        return FileHandler().read(from: "\(id)").catchError { _ in
-            return Request.data(Resource.posts(id).asURLRequest()).flatMap { data in
-                return Response.parse(data: data, to: "\(id)")
+        let fileName = "\(id)"
+        return FileHandler().read(from: fileName).catchError { _ in
+            return Request.data(Resource.posts(id).asURLRequest()).flatMap { data -> Observable<[Post]> in
+                self.fileHandler.write(data, to: fileName)
+                return Response.parse(data: data)
             }
         }
     }
--- ShowcaseApp/Networking/Response.swift
+++ ShowcaseApp/Networking/Response.swift
@@ -3,10 +3,9 @@ import RxSwift
 
 struct Response {
     
-    static func parse<T: Codable>(data: Data, to file: String) -> Observable<T> {
+    static func parse<T: Codable>(data: Data) -> Observable<T> {
         return Observable<T>.create { o in
             do {
-                FileHandler().write(data, to: file)
                 let decodedObject: T = try JSONDecoder().decode(T.self, from: data)
                 o.onNext(decodedObject)
                 o.onCompleted()
